f90c93a0b5996dd67db6ea7aeae6ddf36b392ecb
Hugo Durães <hugoduraes@gmail.com>
2020-11-17 22:35:31 +0000

    update playbooks

--- roles/home/templates/finicky.js
+++ roles/home/templates/finicky.js
@@ -5,17 +5,37 @@ module.exports = {
   handlers: [
     {
       match: finicky.matchDomains([
+        "127.0.0.1",
+        "localhost",
         "tui.com",
         "jsoneditoronline.org",
         "bluejeans.com",
         "zpl.io",
-        "loomio.org",
-        "docs.google.com"
+        "www.loomio.org",
+        "docs.google.com",
+        /.*\.dunelm\.io$/,
+        /.*\.dunelm\.com$/,
+        /^wasp/,
+        /.*\.lucidchart\.com$/,
+        /.*\.atlassian\.com$/,
+        /.*\.slack\.com$/,
+        "dunelm.service-now.com",
+        /.*\.absence\.io$/,
+        "bitbucket.org",
+        /.*\.zoom\.us$/,
+        /.*\.teamretro\.com$/,
+        /(.*\.)?auth0\.com$/,
+        /(.*\.)?sentry.io$/,
+        /^dunelmmill.*/,
+        "funretro.io",
+        "metroretro.io",
+        "gitlab.com",
+        /.*\.mimecast\.com$/
       ]),
       browser: "Firefox"
     },
     {
-      match: /(tuigroup|tui-mobilityhub|tuimobilityhub|mindera|dynatrace|contentful|newrelic|aws\.amazon|beanstalk|zeplin)/,
+      match: /(dunelmmcdev|mercer|tuigroup|tui-mobilityhub|tuimobilityhub|mindera|dynatrace|contentful|newrelic|aws\.amazon|beanstalk|zeplin)/,
       browser: "Firefox"
     }
   ]
